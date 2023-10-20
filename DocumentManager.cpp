397c4af535b119cf258ab4bcd928c45feda8ff9f
hpcatley <hpcatley@gmail.com>
2023-10-20 11:48:06 +1030

    3

--- DocumentManager.cpp
+++ DocumentManager.cpp
@@ -7,11 +7,14 @@ void DocumentManager::addDocument(std::string name, int id, int license_limit) {
 }
 
 void DocumentManager::addPatron(int patronID) {
-    
+    patrons.insert(patronID);
 }
 
 int DocumentManager::search(std::string name) {
-    
+    if(documentNames.find(name) != documentNames.end()) {
+        return documentNames[name].id;
+    }
+    return 0;
 }
 
 bool DocumentManager::borrowDocument(int docid, int patronID) {
