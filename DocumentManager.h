fdbf9e5b6449e45b3bf37574ec7f0c8dce19fc70
hpcatley <hpcatley@gmail.com>
2023-10-20 10:59:52 +1030

    1

---
+++ DocumentManager.h
@@ -0,0 +1,33 @@
+#ifndef DOCUMENTMANAGER_H
+#define DOCUMENTMANAGER_H
+
+#include <unordered_map>
+#include <string>
+#include <set>
+
+class Document {
+public:
+    std::string name;
+    int id;
+    int license_limit;
+    int borrowed_count;
+
+    Document(std::string n, int i, int l);
+};
+
+class DocumentManager {
+private:
+    std::unordered_map<std::string, Document> documentNames;
+    std::unordered_map<int, Document> documentIDs;
+    std::set<int> patrons;
+    std::unordered_map<int, std::set<int>> patron_borrowed_docs;
+
+public:
+    void addDocument(std::string name, int id, int license_limit);
+    void addPatron(int patronID);
+    int search(std::string name);
+    bool borrowDocument(int docid, int patronID);
+    void returnDocument(int docid, int patronID);
+};
+
+#endif // DOCUMENTMANAGER_H
